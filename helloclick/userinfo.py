7a10b29dbaef3356a38daba38cc647a462c8dae1
Ben Hubsch <bah37@duke.edu>
2017-06-20 21:08:10 -0700

    Removing files

--- helloclick/userinfo.py
+++
@@ -1,8 +0,0 @@
-# paths specified upon install
-paths = ['/Users/benhubsch/Documents', 
-         '/Users/benhubsch/Desktop',
-         '/Users/benhubsch/Downloads',
-         '/Users/benhubsch/Dropbox']
-
-# preferred text editor
-editor = 'atom'
